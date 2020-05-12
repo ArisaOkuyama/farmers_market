d6739170689eb9a89bc30dd48d1ff6bbed9fc8f0
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-12 12:10:02 +1000

    added category favorite farm review model

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,2 +1,8 @@
 class Farm < ApplicationRecord
+    has_many :products, dependent: :destroy
+    has_many :reviews
+    belongs_to :user, dependent: :destroy
+
+    validates :user, presence: true
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+    belongs_to :farm
+    has_one :category
     has_one_attached :picture
 end
