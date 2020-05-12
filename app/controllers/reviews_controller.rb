13598f62e5f34469231cc2bcca59fb64a6218196
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-12 19:54:49 +1000

    added reviewing function for farmer

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,31 +10,33 @@ class ReviewsController < ApplicationController
   # GET /reviews/1
   # GET /reviews/1.json
   def show
+    @reviews = Review.find(params[:review_id])
   end
 
   # GET /reviews/new
   def new
+    @farmer = Farmer.find(params[:farmer_id])
     @review = Review.new
+
   end
 
   # GET /reviews/1/edit
   def edit
   end
-
+  
   # POST /reviews
   # POST /reviews.json
   def create
+    @farmer = Farmer.find(params[:farmer_id])
     @review = Review.new(review_params)
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    @review.user = current_user
+    @review.farmer = @farmer
+    if @review.save
+      redirect_to farmer_path(@farmer.id), notice: 'Review was successfully created.' 
+    else
+      redirect_to root_path, notice: "Coundnot save your reveiwe"
     end
+    
   end
 
   # PATCH/PUT /reviews/1
@@ -69,6 +71,6 @@ class ReviewsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def review_params
-      params.fetch(:review, {})
+      params.require(:review).permit(:comment, :rating)
     end
 end
