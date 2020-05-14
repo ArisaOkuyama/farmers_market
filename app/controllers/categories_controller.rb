4c724f8ec5c65480935098767997bfb3c0199723
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-14 17:19:25 +1000

    added category create page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,10 +4,23 @@ class CategoriesController < ApplicationController
   end
 
   def create
-
+    @category = Category.new(category_params)
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to root_path, notice: 'category was successfully created.' }
+      else
+        redirect_to root_path,  notice: :unprocessable_entity
+      end
+    end
   end
 
   def delete
 
+  end
+
+  private
+
+  def category_params
+    params.permit(:name)
   end
 end
