2a7ecfda8cc7666024d159765f1f0d8ad7045a6d
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-18 14:51:03 +1000

    added css to farmers profile and added users picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :state, :address, :phone])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :state, :address, :phone])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:state, :address, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:state, :address, :phone, :users_picture])
   end
 end
