5b921f27ae985071a1cf68bbf5a19b039923d5df
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-18 15:04:05 +1000

    fixed marge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,7 @@ class User < ApplicationRecord
 
     has_many :reviews, dependent: :destroy
     has_one :farmer
-<<<<<<< HEAD
-    
-=======
     has_one_attached :users_picture
->>>>>>> 00b96b0e4517856c6a48c53ec2774b477512a283
     has_many :favorite_farmers, dependent: :destroy
     has_many :fav_farmes, through: :favorite_farmers, class_name: "Farmer", source: :favorited
     has_many :order_history
