f2604395161d2f2f835220d1a5ec68695400c3dd
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-18 14:19:22 +1000

    changed css for comments page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
     has_many :reviews, dependent: :destroy
     has_one :farmer
+    
     has_many :favorite_farmers, dependent: :destroy
     has_many :fav_farmes, through: :favorite_farmers, class_name: "Farmer", source: :favorited
     has_many :order_history
