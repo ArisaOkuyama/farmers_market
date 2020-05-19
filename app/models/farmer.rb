f83fd763d01c72aadad8a12002eea10dc07f1c8f
ArisaOkuyama <arisaokuyama@gmail.com>
2020-05-19 18:25:00 +1000

    added images

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,9 +1,11 @@
 class Farmer < ApplicationRecord
+    
     has_many :products, dependent: :destroy
     has_many :reviews, dependent: :destroy
     has_one_attached :farmers_picture
     belongs_to :user
     has_many :favorite_farmers
+
 
     validates :farm_name, presence: true
     validates :first_name, presence: true
